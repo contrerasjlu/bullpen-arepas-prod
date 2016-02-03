1d565391b767cb6fc40ab695aa6c26fd070d6a03
Jorge Luis Contreras Romero <ingjorgecontreras@gmail.com>
2016-02-02 21:14:17 -0430

    Rest 0.9.1

--- ordertogo/url.py
+++ ordertogo/url.py
@@ -3,13 +3,13 @@ from ordertogo import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    url(r'^categories/$', views.Category.as_view()),
-    #url(r'^products/$', views.Product.as_view()),
-    url(r'^categories/(?P<CatId>[0-9]+)/products/$', views.Product_By_Category),
-    url(r'^categories/(?P<CatId>[0-9]+)/products/(?P<ProdId>[0-9]+)/$', views.Product_Detail),
+    url(r'^onthegame/categories/$', views.api_WebCategories.as_view()),
+    url(r'^onthegame/categories/(?P<CatId>[0-9]+)/$', views.api_WebCategoryDetails.as_view()),
+    url(r'^onthegame/categories/(?P<CatId>[0-9]+)/products/$', views.api_WebProducts.as_view()),
+    url(r'^onthegame/categories/(?P<CatId>[0-9]+)/products/(?P<ProdId>[0-9]+)/$', views.api_WebProductsDetails.as_view()),
 
     url(r'^texts/$', views.Texts.as_view()),
-    url(r'^texts/(?P<code>[\w]+)/$', views.TextDetail.as_view())
+    url(r'^texts/(?P<code>[\w]+)/$', views.TextDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
